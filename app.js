008d0f6eb1898d843798e7194c7d59d96a31ab22
Camilulah <camilavd045@gmail.com>
2022-09-26 19:39:48 -0300

    All

---
+++ app.js
@@ -0,0 +1,26 @@
+const { dir } = require('console');
+const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+
+app.get('/home', function (req, res){
+    res.sendFile((__dirname + '/views/home.html'));
+})
+
+app.get('/register', function (req, res){
+    res.sendFile((__dirname + '/views/register.html'));
+})
+
+app.get('/login', function (req, res){
+    res.sendFile((__dirname + '/views/login.html'));
+})
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando');
+});
